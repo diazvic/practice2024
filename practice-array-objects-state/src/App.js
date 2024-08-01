3f5dd678ab723e8b322be84e5a008b406af4ca9a
Victoria Diaz <65260980+diazvic@users.noreply.github.com>
2024-08-01 10:27:05 +0200

    fc

--- practice-array-objects-state/src/App.js
+++ practice-array-objects-state/src/App.js
@@ -1,68 +1,81 @@
-import "./App.css";
 import { useState } from "react";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 const App = () => {
-	const [cars, setCars] = useState([]);
-	const [carYear, setCarYear] = useState(new Date().getFullYear());
-	const [carMake, setCarMake] = useState("");
-	const [carModel, setCarModel] = useState("");
-
-	const handleAddCar = () => {
-		const newCar = { year: carYear, make: carMake, model: carModel };
-
-		setCars((c) => [...c, newCar]);
-
-		setCarYear(new Date().getFullYear());
-		setCarMake("");
-		setCarModel("");
+	const defaultCategorias = {
+		categorias: [
+			{ id: 1, nombre: "comidas" },
+			{ id: 2, nombre: "servicios" },
+			{ id: 3, nombre: "salidas" },
+			{ id: 4, nombre: "educación" },
+			{ id: 5, nombre: "transporte" },
+			{ id: 6, nombre: "trabajo" },
+		],
+		operaciones: [],
 	};
 
-	const handleRemoveCar = (index) => {
-		setCars((c) => c.filter((_, i) => i !== index));
+	const [data, setData] = useLocalStorage("ahorradas", defaultCategorias);
+	const [categorias, setcategorias] = useState(data.categorias);
+	const [nombreCategoria, setNombreCategoria] = useState("");
+
+	// const handleAddCategoria = () => {
+	// 	const newCategoria = { nombre: nombreCategoria };
+	// 	setcategorias((categorias) => [...categorias, newCategoria]);
+	// 	// en este ejemplo podria hacer una copia de mi data con newdata y sumarle +1 con el index ??//
+	// 	setNombreCategoria("");
+	// };
+
+	const handleAddCategoria = () => {
+		// Crear el nuevo objeto de categoría con un id asignado
+		const newCategoria = {
+			nombre: nombreCategoria,
+		};
+
+		// Actualizar el estado de categorias
+		setcategorias((categorias) => [...categorias, newCategoria]);
+
+		// Actualizar el estado de data y guardar en el local storage
+		const updatedData = {
+			...data,
+			categorias: [...data.categorias, newCategoria],
+		};
+		setData(updatedData);
+
+		// Limpiar el campo de nombre de la categoría
+		setNombreCategoria("");
 	};
 
-	const handleYearChange = (event) => {
-		setCarYear(event.target.value);
+	// Elimina la categoria usando el index
+	const handleRemoveCategoria = (index) => {
+		//para poder ver el numero en el array
+		setcategorias((categorias) => categorias.filter((_, i) => i !== index));
 	};
-
-	const handleMakeChange = (event) => {
-		setCarMake(event.target.value);
-	};
-	const handleModelChange = (event) => {
-		setCarModel(event.target.value);
+	// Voy setteando nombreCategoria en base a lo que voy escribiendo
+	const handleNombreCategoriaChange = (event) => {
+		setNombreCategoria(event.target.value);
 	};
 
 	return (
 		<div>
-			Practice
 			<h2>List of Car Objects</h2>
 			<ul>
-				{cars.map((car, index) => (
-					<li
-						key={index}
-						style={{ display: "flex", alignItems: "center", gap: "10px" }}
-					>
-						{car.year} {car.make} {car.model}
-						<button onClick={() => handleRemoveCar(index)}>Remove</button>
+				{categorias.map((categoria, index) => (
+					<li key={index}>
+						{categoria.nombre}
+						<button onClick={() => handleRemoveCategoria(index)}>
+							Eliminar
+						</button>
 					</li>
 				))}
 			</ul>
-			<input type="number" value={carYear} onChange={handleYearChange} />
 			<input
 				type="text"
-				value={carMake}
-				onChange={handleMakeChange}
-				placeholder="Enter car make"
+				value={nombreCategoria}
+				onChange={handleNombreCategoriaChange}
 			/>
-			<input
-				type="text"
-				value={carModel}
-				onChange={handleModelChange}
-				placeholder="Enter car model"
-			/>
-			<button onClick={handleAddCar}>Add Car</button>
+
+			<button onClick={handleAddCategoria}>Agregar Categoria</button>
 		</div>
 	);
 };
-
 export default App;
